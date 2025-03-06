f3a8811bad700082e0c7e4f619edd6bd022cd8c3
IsabellyL-dev <isabelly.lmoura@gmail.com>
2025-03-06 19:49:31 -0300

    criaçao de aulas e exercicios

---
+++ aula06/anotacoesOperadores.js
@@ -0,0 +1,25 @@
+// auto-atribuiçoes
+var n = 3
+n = n + 4
+console.log(n)
+var n2 = 19
+n2 = n2 ** 2
+console.log(n2)
+// auto-atrbuição simplificada
+// n += 4
+// n **=2
+var n3 = 3
+n3 += 4
+console.log(n3)
+var n4 = 19
+n4 **= 2
+console.log(n4)
+// Incrementação 
+// n ++
+// n --
+var n5 = 3
+n5 ++ 
+console.log(n5)
+var n6 = 3
+n6 --
+console.log(n6)
---
+++ aula06/anotacoesOperadores2.js
@@ -0,0 +1,21 @@
+
+// operador ternario: pergunta? true : false
+var media = 5
+media > 4? console.log('APROVADO') : console.log('REPROVADO')
+var idade = 17
+idade >= 18? console.log('MAIOR DE IDADE') : console.log('MENOR DE IDADE')
+
+// operador relacional
+// >= maior ou igual que
+// <= menor ou igual que 
+//  != diferença
+var n1 = 3 
+var n2 = 3
+var s = n1 <= n2
+console.log(s) 
+
+var num = 5
+var num2 = 7
+var resultado = num == num2
+console.log(resultado)
+
